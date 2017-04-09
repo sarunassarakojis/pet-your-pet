9401c2f8949dfd05864fcb38b8dc735a123da82f
sarunassarakojis <sarakojis.sarunas9@gmail.com>
2017-04-09 18:44:24 +0300

    Add several scripts for movement and camera control

---
+++ pet-your-pet/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 6f;
+    private Vector3 movement;
+    private Animator animator;
+    private Rigidbody playerRigidbody;
+    private int floorMask;
+
+    void Awake()
+    {
+        animator = GetComponent<Animator>();
+        playerRigidbody = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        Move(horizontal, vertical);
+        RotatePlayer(horizontal, vertical);
+        AnimatePlayer(horizontal, vertical);
+    }
+
+    void Move(float h, float v)
+    {
+        movement.Set(h, 0f, v);
+        movement = movement.normalized * speed * Time.deltaTime;
+
+        playerRigidbody.MovePosition(transform.position + movement);
+    }
+
+    void RotatePlayer(float horizontal, float vertical)
+    {
+
+    }
+
+    void AnimatePlayer(float horizontal, float vertical)
+    {
+        animator.SetBool("IsWalking", horizontal != 0 || vertical != 0);
+    }
+}
