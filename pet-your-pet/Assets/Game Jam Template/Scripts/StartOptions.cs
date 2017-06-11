f80a29ad68addd7b0ff1d80fb67af3dc83058bc3
sarunassarakojis <sarakojis.sarunas9@gmail.com>
2017-06-12 02:58:34 +0300

    back to main menu/restart added

--- pet-your-pet/Assets/Game Jam Template/Scripts/StartOptions.cs
+++ pet-your-pet/Assets/Game Jam Template/Scripts/StartOptions.cs
@@ -6,9 +6,6 @@ using UnityEngine.SceneManagement;
 
 public class StartOptions : MonoBehaviour
 {
-
-
-
     public int sceneToStart = 1;                                        //Index number in build settings of scene to load if changeScenes is true
     public bool changeScenes;                                           //If true, load a new scene when Start is pressed, if false, fade out UI and continue in single scene
     public bool changeMusicOnStart;                                     //Choose whether to continue playing menu music or start a new music clip
@@ -122,7 +119,6 @@ public class StartOptions : MonoBehaviour
         //Set trigger for animator to start animation fading out Menu UI
         animMenuAlpha.SetTrigger("fade");
         Invoke("HideDelayed", fadeAlphaAnimationClip.length);
-        Debug.Log("Game started in same scene! Put your game starting stuff here.");
     }
 
 
