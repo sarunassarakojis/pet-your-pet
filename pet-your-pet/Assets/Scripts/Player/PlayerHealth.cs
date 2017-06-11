ada72fa39fc84b09cf25acc8e348f2e4744a3748
sarunassarakojis <sarakojis.sarunas9@gmail.com>
2017-06-11 21:45:36 +0300

    Add health bar

---
+++ pet-your-pet/Assets/Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int startingHealth = 100;
+    public int currentHealth;
+    public Slider healthSlider;
+    public Image damageImage;
+    public AudioClip deathClip;
+    public float flashSpeed = 5f;
+    public Color flashColour = new Color(1f, 0f, 0f, 0.1f);
+
+    private PlayerUserControl playerControl;
+    private AudioSource playerAudio;
+    private bool isDead;
+    private bool damaged;
+
+    void Awake()
+    {
+        playerAudio = GetComponent<AudioSource>();
+        playerControl = GetComponent<PlayerUserControl>();
+
+        currentHealth = startingHealth;
+    }
+
+    void Update()
+    {
+        if (damaged)
+        {
+            damageImage.color = flashColour;
+        }
+        else
+        {
+            damageImage.color = Color.Lerp(damageImage.color, Color.clear, flashSpeed * Time.deltaTime);
+        }
+
+        damaged = false;
+    }
+
+    public void TakeDamage(int amount)
+    {
+        damaged = true;
+        currentHealth -= amount;
+        healthSlider.value = currentHealth;
+
+        playerAudio.Play();
+
+        if (currentHealth <= 0 && !isDead)
+        {
+            Death();
+        }
+    }
+
+    void Death()
+    {
+        isDead = true;
+        playerControl.enabled = false;
+
+        playerAudio.clip = deathClip;
+        playerAudio.Play();
+    }
+}
--- pet-your-pet/Assets/Scripts/Player/PlayerUserControl.cs
+++ pet-your-pet/Assets/Scripts/Player/PlayerUserControl.cs
@@ -1,67 +1,63 @@
 using UnityEngine;
 using UnityStandardAssets.CrossPlatformInput;
-using UnityEngine.UI;
 
-namespace UnityStandardAssets.Characters.ThirdPerson
+[RequireComponent(typeof(PlayerCharacter))]
+public class PlayerUserControl : MonoBehaviour
 {
-    [RequireComponent(typeof(PlayerCharacter))]
-    public class PlayerUserControl : MonoBehaviour
+    private PlayerCharacter m_Character; // A reference to the ThirdPersonCharacter on the object
+    private Transform m_Cam;                  // A reference to the main camera in the scenes transform
+    private Vector3 m_CamForward;             // The current forward direction of the camera
+    private Vector3 m_Move;
+    private bool m_Jump;                      // the world-relative desired move direction, calculated from the camForward and user input.
+
+    private void Start()
     {
-        private PlayerCharacter m_Character; // A reference to the ThirdPersonCharacter on the object
-        private Transform m_Cam;                  // A reference to the main camera in the scenes transform
-        private Vector3 m_CamForward;             // The current forward direction of the camera
-        private Vector3 m_Move;
-        private bool m_Jump;                      // the world-relative desired move direction, calculated from the camForward and user input.
-
-        private void Start()
+        if (Camera.main != null)
         {
-            if (Camera.main != null)
-            {
-                m_Cam = Camera.main.transform;
-            }
-            else
-            {
-                Debug.LogWarning("Warning: no main camera found. Third person character needs a Camera tagged \"MainCamera\", for camera-relative controls.", gameObject);
-            }
-
-            m_Character = GetComponent<PlayerCharacter>();
+            m_Cam = Camera.main.transform;
+        }
+        else
+        {
+            Debug.LogWarning("Warning: no main camera found. Third person character needs a Camera tagged \"MainCamera\", for camera-relative controls.", gameObject);
         }
 
-        private void Update()
+        m_Character = GetComponent<PlayerCharacter>();
+    }
+
+    private void Update()
+    {
+        if (!m_Jump)
         {
-            if (!m_Jump)
-            {
-                m_Jump = CrossPlatformInputManager.GetButtonDown("Jump");
-            }
+            m_Jump = CrossPlatformInputManager.GetButtonDown("Jump");
         }
+    }
 
-        private void FixedUpdate()
+    private void FixedUpdate()
+    {
+        // read inputs
+        float h = CrossPlatformInputManager.GetAxis("Horizontal");
+        float v = CrossPlatformInputManager.GetAxis("Vertical");
+        bool crouch = Input.GetKey(KeyCode.C);
+
+        // calculate move direction to pass to character
+        if (m_Cam != null)
         {
-            // read inputs
-            float h = CrossPlatformInputManager.GetAxis("Horizontal");
-            float v = CrossPlatformInputManager.GetAxis("Vertical");
-            bool crouch = Input.GetKey(KeyCode.C);
-
-            // calculate move direction to pass to character
-            if (m_Cam != null)
-            {
-                // calculate camera relative direction to move:
-                m_CamForward = Vector3.Scale(m_Cam.forward, new Vector3(1, 0, 1)).normalized;
-                m_Move = v * m_CamForward + h * m_Cam.right;
-            }
-            else
-            {
-                // we use world-relative directions in the case of no main camera
-                m_Move = v * Vector3.forward + h * Vector3.right;
-            }
+            // calculate camera relative direction to move:
+            m_CamForward = Vector3.Scale(m_Cam.forward, new Vector3(1, 0, 1)).normalized;
+            m_Move = v * m_CamForward + h * m_Cam.right;
+        }
+        else
+        {
+            // we use world-relative directions in the case of no main camera
+            m_Move = v * Vector3.forward + h * Vector3.right;
+        }
 #if !MOBILE_INPUT
-            // walk speed multiplier
-            if (Input.GetKey(KeyCode.LeftShift)) m_Move *= 0.5f;
+        // walk speed multiplier
+        if (Input.GetKey(KeyCode.LeftShift)) m_Move *= 0.5f;
 #endif
 
-            // pass all parameters to the character control script
-            m_Character.Move(m_Move, crouch, m_Jump);
-            m_Jump = false;
-        }
+        // pass all parameters to the character control script
+        m_Character.Move(m_Move, crouch, m_Jump);
+        m_Jump = false;
     }
 }
